d8c41ba7a234d0b87b41826d5ed26e3937ddb837
huwaidanisrina <danishuwaida@gmail.com>
2018-08-27 17:08:48 +0700

    projek-pertama

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama : Huwaida Nisrina Najla</Text>
+        <Text>Kelas : XI RPL 4</Text>
+        <Text>No Absen : 14</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

23d1fa489b4e7cf259c3b49ca086e55eb4d9676a
huwaidanisrina <danishuwaida@gmail.com>
2018-08-30 15:53:45 +0700

    Component Insert

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import Judul from './Component/Judul';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      	<Judul/>
         <Text>Nama : Huwaida Nisrina Najla</Text>
         <Text>Kelas : XI RPL 4</Text>
         <Text>No Absen : 14</Text>
---
+++ Component/Judul.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+class Judul extends React.Component{
+	render(){
+		return (
+				<Text style={salon.Judul}>BIODATA</Text>
+		)
+	}
+}
+
+const salon = {
+	Judul: {
+		color: '#f00',
+		fontSize: 50
+	}
+}
+export default Judul;
